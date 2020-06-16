98c8b025b02b4ed5d9e5f234d8baf4afc8ae4f8c
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-15 20:50:48 -0700

    YouTubeImportV1.py
    An archive of the first version of the YouTube import script.

---
+++ AudiBassManager/Import/Sites/OldVersions/YouTubeImportV1.py
@@ -0,0 +1,35 @@
+# Import content from YouTube
+
+# Link formats:
+
+# Long link
+print("Long link: https://www.youtube.com/watch?v=jNQXAC9IVRw")
+
+# Short link
+print("Short link: https://youtu.be/jNQXAC9IVRw")
+
+# Removed:
+"""
+www.
+.com
+watch?v=
+"""
+
+# Mobile links
+
+# Coming soon
+
+# Computer links with the attribute
+
+# Coming soon
+
+'''
+File info
+
+File type: Python (*.py)
+
+Line count: 36
+
+Version: 1 (June 15h 2020 at 8:50 pm)
+'''
+# End of script
