47a014f4cf895c8c4f72ad7aca93a5c2e635def7
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-15 21:16:02 -0700

    PROFILE_WIZARD.py
    The first version of the profile wizard script.

---
+++ AudiBassManager/Users/PROFILE_WIZARD.py
@@ -0,0 +1,9 @@
+# Start of script
+print("The profile wizard is out of magic! Please use a more updated version of the AudiBassManager software to use this wizard")
+noMoreExit = input("Press [ENTER] key to close")
+print("You closed the wizards door, and the wizards house is gone. If you are still reading this, close out of the window manually through the close button, or use a --taskkill command on this process.")
+# End of script
+"""
+File version:
+1 (June 15th 2020 at 9:16 pm)
+"""
