04a7f7041ec962a19e3ed909a34e37362458b83d
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-15 21:00:50 -0700

    README.py
    Hoping a Python README file will work.

---
+++ AudiBassManager/Installers/Win64x/README.py
@@ -0,0 +1,5 @@
+print("DO NOT RUN THESE FILES YET")
+
+print("These files are currently just for display, they do not work. You should also be able to tell this by the incredibly small file sizes")
+
+# End
